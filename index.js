cbcd7abc1a353694ffcb5ae2a4a75d9784e63d95
mohinimraut7 <mohinimraut7@gmail.com>
2025-07-03 11:01:45 +0530

    env changes

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ mongoose
     console.log("Database connected");
   });
 app.use(express.json({ limit: '100mb' }));
+
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
   app.use('/api',addUserRoutes)
   app.use('/api',addRoleRoutes)
